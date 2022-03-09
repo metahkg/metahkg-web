1b6eb71a7e696ab8c60a7054fee7cc7d5dfa3a7e
wcyat <sleep@wcyat.me>
2022-03-09 15:20:14 +0800

    force all transparent

--- src/components/texteditor.tsx
+++ src/components/texteditor.tsx
@@ -23,16 +23,16 @@ export default function TextEditor(props: {
           menubar: true,
         },
         imagetools_cors_hosts: ["picsum.photos"],
-        quickbars_selection_toolbar: 'cut copy paste | bold italic | formatselect | quicklink template',
-        quickbars_insert_toolbar: 'emoticons | quicklink image | template anchor codesample',
+        quickbars_selection_toolbar: 'cut copy paste | formatselect | quicklink template',
+        quickbars_insert_toolbar: '',
         menubar: "file edit view insert format tools table",
         plugins:
           "print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons",
         toolbar:
-          "undo redo | link image | formatselect | bold italic underline strikethrough forecolor backcolor | emoticons | numlist bullist | alignleft aligncenter alignright alignjustify | outdent indent | fontsizeselect | media table removeformat pagebreak | template charmap | fullscreen preview save print | insertfile anchor codesample | ltr rtl | help",
+          "undo redo | link image template codesample | emoticons | bold italic underline strikethrough forecolor backcolor | numlist bullist | alignleft aligncenter alignright alignjustify | outdent indent | formatselect fontsizeselect | media table removeformat pagebreak | template charmap | fullscreen preview save print | insertfile anchor codesample | ltr rtl | help",
         toolbar_sticky: true,
         templates: [
-          {title: "Quote", description: "Quote text.", content: `<blockquote style="color: #aca9a9; border-left: 2px solid #aca9a9; margin-left: 0"><div style="margin-left: 15px">quote</div></blockquote><p></p>`}
+          {title: "Quote", description: "Add a quote.", content: `<blockquote style="color: #aca9a9; border-left: 2px solid #aca9a9; margin-left: 0"><div style="margin-left: 15px">quote</div></blockquote><p></p>`}
         ],
         autosave_ask_before_unload: true,
         autosave_interval: "30s",
