a28e5a3fe82369673781893c2094c6465edd7097
sourcetreeeeeeeeeeeeeee <sourcetreeeeeeeeeeeeeee@gmail.com>
2022-04-08 23:15:24 +0800

    fall back default

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: process.env.BACKEND_URL,
+      target: process.env.BACKEND_URL || "https://dev.metahkg.org",
       changeOrigin: true,
     })
   );
