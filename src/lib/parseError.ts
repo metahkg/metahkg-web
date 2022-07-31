8fb5fb74561566ff27a618700e2cf22e4e7f3e48
wcyat <wcyat@wcyat.me>
2022-07-31 14:12:22 +0800

    fix parse error

--- src/lib/parseError.ts
+++ src/lib/parseError.ts
@@ -1,11 +1,14 @@
 import { AxiosError } from "axios";
 
-export function parseError(err: AxiosError<any>): string {
+export function parseError(err: AxiosError<any> | any): string {
     try {
-        if (err.response?.data?.error) return err.response.data.error;
+        if (typeof err.response?.data?.error === "string") return err.response.data.error;
+        if (typeof err.error === "string") return err.error;
 
-        if (err.response?.data && err.response.data?.length < 50)
+        if (typeof err.response?.data === "string" && err.response.data?.length < 50)
             return err.response.data;
+
+        if (typeof err === "string" && err?.length < 50) return err;
 
         if (err.response?.statusText)
             return `${err.response?.status} ${err.response?.statusText}`;
