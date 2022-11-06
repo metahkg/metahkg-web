f2bbe4bc609c52e30d1587b2d70d0b96a07eb7da
wcyat <wcyat@wcyat.me>
2022-11-06 13:55:45 +0800

    include license terms in all files

--- src/lib/switch.tsx
+++ src/lib/switch.tsx
@@ -1,4 +1,19 @@
-import { styled, Switch, SwitchProps } from "@mui/material";
+/*
+ Copyright (C) 2022-present Metahkg Contributors
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 /*
 The MIT License (MIT)
 
@@ -22,6 +37,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
+import { styled, Switch, SwitchProps } from "@mui/material";
 export const Android12Switch = styled(Switch)(({ theme }) => ({
     padding: 8,
     "& .MuiSwitch-track": {
--- src/types/notification.ts
+++ src/types/notification.ts
@@ -1,8 +1,3 @@
 import { severity } from "./severity";
 
 export type notification = { open: boolean; text: string; severity?: severity };
-
-export type RemoteNotification = {
-    title: string;
-    option: NotificationOptions;
-};
