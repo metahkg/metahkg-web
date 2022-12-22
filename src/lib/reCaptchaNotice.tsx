12d5f4964192a250791ef918ad9ca0e123ba8e11
wcyat <wcyat@wcyat.me>
2022-12-22 17:44:41 +0800

    update: recaptcha notice

--- src/lib/reCaptchaNotice.tsx
+++ src/lib/reCaptchaNotice.tsx
@@ -4,7 +4,7 @@ export default function ReCaptchaNotice(props: { className?: string }) {
     const { className } = props;
     return (
         <p className={`text-[12px] text-metahkg-grey ${className}`}>
-            This site is protected by reCAPTCHA and the Google{" "}
+            Metahkg is protected by reCAPTCHA. The Google{" "}
             <a className="inline" href="https://policies.google.com/privacy">
                 Privacy Policy
             </a>{" "}
