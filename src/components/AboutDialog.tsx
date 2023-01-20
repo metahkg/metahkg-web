9decc0f508f121d644c49f9f59b104c2ff527e21
wcyat <wcyat@wcyat.me>
2023-01-20 22:51:16 +0800

    about dialog: add third-party licenses & logo copyright

--- src/components/AboutDialog.tsx
+++ src/components/AboutDialog.tsx
@@ -14,39 +14,48 @@ export function AboutDialog(props: {
                     Metahkg is a free and open source lihkg-style forum.
                 </p>
                 <h3>Source code</h3>
-                <a
-                    href="https://gitlab.com/metahkg/metahkg"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Metahkg Main Repository
-                </a>
-                <a
-                    href="https://gitlab.com/metahkg/metahkg-web"
-                    className="mt-[10px]"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Metahkg Web App
-                </a>
+                <p className="my-0">
+                    <a
+                        href="https://gitlab.com/metahkg/metahkg"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Metahkg Main Repository
+                    </a>
+                </p>
+                <p className="mb-0">
+                    <a
+                        href="https://gitlab.com/metahkg/metahkg-web"
+                        className="mt-[10px]"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Metahkg Web App
+                    </a>
+                </p>
                 {process.env.REACT_APP_version ? (
                     <React.Fragment>
                         <h3>Version</h3>
-                        <a
-                            href={`https://gitlab.com/metahkg/metahkg-web/-/tree/${process.env.REACT_APP_version}`}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            v{process.env.REACT_APP_version}
-                        </a>
-                        <a
-                            href={`https://gitlab.com/metahkg/metahkg-web/-/blob/${process.env.REACT_APP_version}/CHANGELOG.md`}
-                            className="mt-[10px]"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            CHANGELOG
-                        </a>
+                        <p className="my-0">
+                            <a
+                                href={`https://gitlab.com/metahkg/metahkg-web/-/tree/${process.env.REACT_APP_version}`}
+                                className="inline"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                v{process.env.REACT_APP_version}
+                            </a>
+                        </p>
+                        <p className="mb-0">
+                            <a
+                                href={`https://gitlab.com/metahkg/metahkg-web/-/blob/${process.env.REACT_APP_version}/CHANGELOG.md`}
+                                className="inline"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                CHANGELOG
+                            </a>
+                        </p>
                     </React.Fragment>
                 ) : (
                     <></>
@@ -54,13 +63,16 @@ export function AboutDialog(props: {
                 {process.env.REACT_APP_build ? (
                     <React.Fragment>
                         <h3>Build</h3>
-                        <a
-                            href={`https://gitlab.com/metahkg/metahkg-web/-/commit/${process.env.REACT_APP_build}`}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            {process.env.REACT_APP_build}
-                        </a>
+                        <p className="my-0">
+                            <a
+                                href={`https://gitlab.com/metahkg/metahkg-web/-/commit/${process.env.REACT_APP_build}`}
+                                className="inline"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                {process.env.REACT_APP_build}
+                            </a>
+                        </p>
                     </React.Fragment>
                 ) : (
                     <></>
@@ -78,9 +90,39 @@ export function AboutDialog(props: {
                     Metahkg Copyright (c) 2022-present Metahkg Contributors
                 </p>
                 <p className="mb-0">
-                    <a href="https://gitlab.com/metahkg/metahkg/-/tree/master/LICENSE.md">
+                    <a
+                        href="https://gitlab.com/metahkg/metahkg/-/tree/master/LICENSE.md"
+                        className="inline"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         AGPL-3.0-or-later
                     </a>
+                </p>
+                <p className="mb-0">
+                    <a
+                        href="/third-party-licenses.txt"
+                        className="inline"
+                        target="_blank"
+                    >
+                        Third-party Licenses
+                    </a>
+                </p>
+                <h4>Logo</h4>
+                <p className="my-0">
+                    Metahkg logo Copyright (c) 2022 "white card", CC-BY-4.0
+                </p>
+                <p>
+                    See{" "}
+                    <a
+                        href="https://gitlab.com/metahkg/metahkg-web/-/tree/master/public/images/LICENSE"
+                        className="inline"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        metahkg-web@public/images/LICENSE
+                    </a>{" "}
+                    for more information.
                 </p>
             </Box>
         </PopUp>
