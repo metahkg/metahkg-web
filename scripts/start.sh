0bf3e80771e09790dc5ade1412a307f38476898b
wcyat <wcyat@wcyat.me>
2023-02-12 21:13:26 +0800

    fix: disable clipboard and port switching for serve

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,4 +13,4 @@ if [ "${GCM_SENDER_ID}" != "" ]; \
 then sed -i "s/{GCM_SENDER_ID}/${GCM_SENDER_ID}/g" build/manifest.json; fi; \
 if [ "${PORT}" = "" ] && [ "${port}" != "" ]; \
 then export PORT="${port}"; fi; \
-if [ "${env}" = "dev" ]; then yarn start:react; else (serve -s -l "${PORT:-8080}" || serve -s); fi;
+if [ "${env}" = "dev" ]; then yarn start:react; else serve -s -l "${PORT:-8080}" --no-clipboard --no-port-switching; fi;
