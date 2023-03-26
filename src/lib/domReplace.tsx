6828955d2a559258b0c5aff4e8aa0ccf54fe68d2
wcyat <wcyat@wcyat.me>
2023-03-26 23:14:25 +0800

    fix(domReplace): remove console.log

--- src/lib/domReplace.tsx
+++ src/lib/domReplace.tsx
@@ -131,7 +131,6 @@ export function useReplace(params: { quote?: boolean }) {
                                     (i) => i === (firstChild as unknown as Text)?.data
                                 )
                             ) {
-                                console.log(href, href.endsWith(".pdf"));
                                 if (settings.pdfViewer && href.endsWith(".pdf")) {
                                     return (
                                         <>
