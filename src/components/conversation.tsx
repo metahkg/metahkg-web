d2707e83ef9b2286253485775b6d2f0338c5e059
wcyat <wcyat@wcyat.me>
2023-05-25 19:21:55 +0800

    fix(conversation): pagifying problem caused by visibility

--- src/components/conversation.tsx
+++ src/components/conversation.tsx
@@ -233,7 +233,12 @@ function Conversation(props: { id: number }) {
                                             </VisibilitySensor>
                                             <React.Fragment>
                                                 {thread.conversation
-                                                    .slice(index * 25, (index + 1) * 25)
+                                                    .filter(
+                                                        (comment) =>
+                                                            comment.id >
+                                                                (page - 1) * 25 &&
+                                                            comment.id <= page * 25
+                                                    )
                                                     .map(
                                                         (comment) =>
                                                             !("removed" in comment) &&
